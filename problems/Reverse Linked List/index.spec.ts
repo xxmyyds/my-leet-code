0e9ebc2370f8b168603790ed9fe023b73df8e489
Code_xxmyyds <xxmyyds@163.com>
2022-05-04 20:31:49 +0800

    Reverse Linked List

--- problems/Reverse Linked List/index.spec.ts
+++ problems/Reverse Linked List/index.spec.ts
@@ -1,5 +1,51 @@
+import { expect, test, describe } from 'vitest'
+import { reverseList } from './index'
+describe('Reverse Linked List', () => {
+  test('case 1', () => {
+    const a: any = { val: 1 }
+    const b: any = { val: 2 }
+    const c: any = { val: 3 }
+    const d: any = { val: 4 }
+    const e: any = { val: 5 }
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
 
-  import { expect, test, describe } from 'vitest'
-  import {} from './index'
-  describe('Reverse Linked List', () => {})
-  
+    let result = []
+    let p = reverseList(a)
+
+    while (p) {
+      result.push(p.val)
+      p = p.next
+    }
+
+    expect(result).toEqual([5, 4, 3, 2, 1])
+  })
+  test('case 2', () => {
+    const a: any = { val: 1 }
+    const b: any = { val: 2 }
+
+    a.next = b
+
+    let result = []
+    let p = reverseList(a)
+
+    while (p) {
+      result.push(p.val)
+      p = p.next
+    }
+
+    expect(result).toEqual([2, 1])
+  })
+  test('case 3', () => {
+    let result = []
+    let p = reverseList(null)
+
+    while (p) {
+      result.push(p.val)
+      p = p.next
+    }
+    expect(result).toEqual([])
+  })
+})
--- problems/Reverse Linked List/index.ts
+++ problems/Reverse Linked List/index.ts
@@ -1,1 +1,11 @@
-export function fn() {}
+export function reverseList(head: any) {
+  let p1 = head
+  let p2 = null
+  while (p1) {
+    const tmp = p1.next
+    p1.next = p2
+    p2 = p1
+    p1 = tmp
+  }
+  return p2
+}
